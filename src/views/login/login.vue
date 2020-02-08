8aad3477cee57308a09b77bf1ad341ca8051d8ea
hunzi <994937581@qq.com>
2020-02-08 10:15:28 +0800

    登录模块-基础布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,7 +2,15 @@
   <!-- 整体容器 -->
   <div class="login-container">
     <!-- 左侧盒子 -->
-    <div class="left-box"></div>
+    <div class="left-box">
+        <!-- 顶部的标题 -->
+        <div class="title-box">
+            <img src="../../assets/login_logo.png" alt="" class="logo">
+            <span class="title">黑马面面</span>
+            <span class="line"></span>
+            <span class="sub-title">用户登录</span>
+        </div>
+    </div>
     <!-- 右边的图片 -->
     <img src="../../assets/login_banner_ele.png" alt />
   </div>
@@ -16,7 +24,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
 .login-container {
   background: linear-gradient(
     225deg,
@@ -30,10 +38,38 @@ export default {
   align-items: center;
   /* 左右均分 */
   justify-content: space-around;
+
+    // 左侧盒子
+  .left-box {
+    width: 478px;
+    background-color: #f5f5f5;
+    height: 550px;
+
+    // 顶部标题
+    .title-box {
+        display: flex;
+        align-items: center;
+        margin-top: 44px;
+        margin-left: 40px;
+        .logo{
+
+        }
+        .title{
+            font-size: 24px;
+            margin-left: 16px;
+            margin-right: 14px;
+        }
+        .line{
+            width: 1px;
+            height: 28px;
+            background-color: #c7c7c7;
+        }
+        .sub-title{
+            font-size: 21px;
+            margin-left: 12px;
+        }
+    }
+  }
 }
-.login-container .left-box {
-  width: 478px;
-  background-color: #f5f5f5;
-  height: 550px;
-}
+// .login-container
 </style>
